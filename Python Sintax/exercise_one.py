e719bb20838e2d91190d888211c149701fafe5dd
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-03 11:49:15 -0500

    First Module: Python Sintax

---
+++ Python Sintax/exercise_one.py
@@ -0,0 +1,4 @@
+# Solution:
+print("Hello World")
+
+# More Information here: https://www.w3schools.com/python/ref_func_print.asp
---
+++ Python Sintax/exercise_two.py
@@ -0,0 +1,8 @@
+# Solution:
+if 5 > 2:
+  print("Five is greater than two!")
+  
+'''
+The error here is the indentation
+More Information here: https://www.w3schools.com/python/gloss_python_indentation.asp
+'''
