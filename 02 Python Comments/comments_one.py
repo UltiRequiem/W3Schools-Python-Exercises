a02652653418246334ed8b7f5827c3e1ff058d90
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-03 11:55:36 -0500

    Python Comments

---
+++ 02 Python Comments/comments_one.py
@@ -0,0 +1,7 @@
+# Solution:
+# This is a comment
+
+''' 
+# Are used for single line comments
+Read More Here: https://www.w3schools.com/python/python_comments.asp
+'''
---
+++ 02 Python Comments/comments_two.py
@@ -0,0 +1,11 @@
+# Solution:
+'''
+This is a comment
+written in 
+more that just one line
+'''
+
+'''
+Triple quotes are used for comments of more than a single line
+Read More Here: https://www.w3schools.com/python/python_comments.asp 
+'''
