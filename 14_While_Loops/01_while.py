2937c15a7d109897eb6a3d7635685ec754fbb3f3
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-03-18 09:20:43 -0500

    ✨ While Loops

---
+++ 14_While_Loops/01_while.py
@@ -0,0 +1,6 @@
+# Print i as long as i is less than 6.
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
---
+++ 14_While_Loops/02_while.py
@@ -0,0 +1,7 @@
+# Stop the loop if i is 3.
+
+i = 1
+while i < 6:
+    if i == 3:
+        break
+    i += 1
